57aa61db85a5b6353d7041c9c0fbfd49cb092c44
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-03 18:34:57 +0200

    Change directory

--- utils/downloader.py
+++ utils/downloader.py
@@ -6,13 +6,19 @@ Downloads files.
 """
 
 
+import os
+
+
 def download_files(files, destination, verbose):
     """
     This function downloads files to a specified directory. Destination
     is a path to a directory where images will be stored. If verbose is
     True, then the download status is displayed.
     """
-    pass
+    current_directory = os.getcwd()
+    os.chdir(destination)
+
+    os.chdir(current_directory)
 
 
 if __name__ == '__main__':
