0ac64709f9045e01fb08b3fe6adc63b7aa6fa2c0
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-03 17:45:59 +0200

    Add two new modules

---
+++ domainparsers/common.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+Constants shared between parsers. These constants could be turned into classes.
+For example:
+
+class Domain(object):
+    REDDIT = 'reddit'
+    GFYCAT = 'gfycat'
+    ...
+
+So later in the program they can be used as Domain.REDDIT.
+"""
---
+++ domainparsers/reddit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+"""
+Parser for reddit.
+"""
