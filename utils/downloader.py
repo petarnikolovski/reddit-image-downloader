7083dceac7915274beb5089a5bb2a64fa7e3e3d4
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-04 00:34:01 +0200

    Add some download logic

--- utils/downloader.py
+++ utils/downloader.py
@@ -10,6 +10,8 @@ import os
 import sqlite3
 from utils.consoleaccessories import is_valid_path
 from utils.consoleaccessories import clean_path
+from utils.debugtools import count_downloadable_images
+from utils.politeness import get_politeness_factor
 
 
 DB_TEMPLATE = """
@@ -54,9 +56,21 @@ def download_files(files, destination, verbose):
     conn = make_connection(db_path)
     c = conn.cursor()
 
+    # Download, deal with exception, save to db, log things...
+    total = count_downloadable_images(files)
+    currently_downloading = 1
+    while files:
+        file_obj = files.popleft()
+
+        if file_obj['image']['image_url']:
+            if verbose: display_status(file_obj['image']['image_url'], currently_downloading, total)
+
     conn.close()
     os.chdir(current_directory)
 
 
 def make_connection(path):
+    """
+    Make connection to database.
+    """
     return sqlite3.connect(path)
