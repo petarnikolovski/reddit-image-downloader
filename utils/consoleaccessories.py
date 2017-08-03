51e2b7308e9c10713944213e3b21e5dab4f353f0
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-03 19:38:09 +0200

    Correct a syntax error

--- utils/consoleaccessories.py
+++ utils/consoleaccessories.py
@@ -15,7 +15,7 @@ def is_valid_domain(url):
     """
     Checks if the domain is valid for the application.
     """
-    return 'www.reddit.com' in url:
+    return 'www.reddit.com' in url
 
 
 def is_valid_path(path):
