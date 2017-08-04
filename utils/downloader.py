60554ee9b7ceef81debcb4106aa06f605c36af46
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-04 22:18:15 +0200

    Add download skeleton

--- utils/downloader.py
+++ utils/downloader.py
@@ -9,6 +9,10 @@ Downloads files.
 import os
 import sqlite3
 from contextlib import suppress
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from shutil import copyfileobj
 from utils.consoleaccessories import is_valid_path
 from utils.consoleaccessories import clean_path
 from utils.debugtools import count_downloadable_images
@@ -64,20 +68,55 @@ def download_files(files, destination, verbose):
     while files:
         file_obj = files.popleft()
 
-        if file_obj['image']['image_url']:
+        image_url = file_obj['image']['image_url']
+        filename = file_obj['image']['filename']
+        if image_url:
             if verbose: display_status(file_obj['image']['image_url'], currently_downloading, total)
             sldn = file_obj['second_level_domain_name']
             crawl_time = get_politeness_factor(sldn)
 
             # skip downloading if file is already downloaded
-            currently_downloading += 1
+            try:
+                write_file_to_filesystem(image_url, filename)
+            except HTTPError as e:
+                status = e.code
+                print('Could not downloade, error status:', status)
 
+                if status == '404':
+                    print('File not found.')
+                    # log missing, and write to db
+                    currently_downloading += 1
+                elif status == '429':
+                    print('Too many requests were made to the server')
+                    # does not return file at the end of the deque
+                    currently_downloading += 1
+                else:
+                    print('Downloading will be retried later.')
+                    # log this
+                    files.append(file_obj)
+            except URLError as e:
+                print('Something went wrong with connection.')
+                print('Connection may have been refused.')
+            else:
+                pass
+                currently_downloading += 1
+
+            # If two consecutive domains are different, there is no need to
+            # wait for the next download
             with suppress(IndexError):
                 if file_obj['domain'] == files[1]['domain']:
                     sleep(crawl_time)
 
     conn.close()
     os.chdir(current_directory)
+
+
+def write_file_to_filesystem(url, filename):
+    """
+    Write a file to a file system.
+    """
+    with urlopen(url) as r, open(filename, 'wb') as f:
+        copyfileobj(r, f)
 
 
 def display_status(url, currently_at, total):
