42205f801fe42d5b20fe934db198e4ed81c8840b
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-12 22:32:12 +0200

    Remove docstring

--- domainparsers/common.py
+++ domainparsers/common.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 
 
-"""
-Constants shared between parsers. These constants could be turned into classes.
-For example:
-
-class Domain(object):
-    REDDIT = 'reddit'
-    GFYCAT = 'gfycat'
-    ...
-
-So later in the program they could be used as Domain.REDDIT.
-"""
-
-
 class FileFormats(object):
     JPG = '.jpg'
     JPEG = '.jpeg'
