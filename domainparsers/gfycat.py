b8116acdecbc140c8b3c0f8e7fe0c161149827eb
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-12 22:37:59 +0200

    Merge two modules

--- domainparsers/gfycat.py
+++ domainparsers/gfycat.py
@@ -7,7 +7,23 @@ By default, it only downloads .webm format.
 """
 
 
-from domainparsers.internetaccess import make_soup
+from urllib.request import urlopen
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+def make_soup(url, parser='lxml', selenium=False):
+    """
+    Return a soup object. Default parser is lxml. Requests are made by
+    default using url.request library.
+    """
+    if selenium:
+        driver = webdriver.PhantomJS()
+        driver.get(url)
+        html = driver.page_source
+        driver.close()
+        return BeautifulSoup(html, parser)
+    return BeautifulSoup(urlopen(url).read(), parser)
 
 
 def parse_gfycat(url):
