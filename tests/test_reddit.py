7dcda9135b5b2ccc130b5ad6ff9b02a2853f1a07
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-13 02:21:03 +0200

    Test reddit's file formats method

--- tests/test_reddit.py
+++ tests/test_reddit.py
@@ -4,6 +4,7 @@
 import pytest
 from domainparsers.reddit import Reddit
 from domainparsers.reddit import RedditException
+from domainparsers.common import FileFormats
 
 @pytest.fixture
 def test_link():
@@ -15,3 +16,20 @@ def test_invalid_link():
     """
     with pytest.raises(RedditException):
         Reddit('https://reddit.com/')
+
+def test_file_formats(test_link):
+    """
+    Test if the known_file_format method returns correct formats.
+    """
+    url_1 = 'http://i.imgur.com/jedEzFL.jpg'
+    url_2 = 'http://i.imgur.com/jedEzFL.png'
+    url_3 = 'http://i.imgur.com/jedEzFL.webm'
+    url_4 = 'http://i.imgur.com/jedEzFL.m'
+    result_1 = FileFormats.JPG
+    result_2 = FileFormats.PNG
+    result_3 = FileFormats.WEBM
+    result_4 = None
+    assert test_link.known_file_format(url_1) == result_1
+    assert test_link.known_file_format(url_2) == result_2
+    assert test_link.known_file_format(url_3) == result_3
+    assert test_link.known_file_format(url_4) == result_4
