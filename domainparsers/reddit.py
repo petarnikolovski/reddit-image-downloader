6cca77c083202b485708ac57f6f61e8fa7178178
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-14 23:19:51 +0200

    Add print all domains method to Reddit class

--- domainparsers/reddit.py
+++ domainparsers/reddit.py
@@ -255,3 +255,13 @@ class Reddit(object):
         for key, group in groupby(domains):
             grouped_domains.append((len(list(group)), key))
         return grouped_domains
+
+    def print_all_domains(self):
+        """
+        Print list of all domains appearing in /r group. First element is
+        number of times a domain appears in a posts list, second element is
+        corresponding domain name.
+        """
+        domains = self.get_all_domains()
+        for domain in sorted(domains, key=lambda x: x[0]):
+            print(domain[0], domain[1])
--- utils/debugtools.py
+++ utils/debugtools.py
@@ -6,16 +6,6 @@ Debugging, and analysis tools.
 """
 
 
-def print_all_domains(domains):
-    """
-    Print list of all domains appearing in /r group. First element is
-    number of times a domain appears in a posts list, second element is
-    corresponding domain name.
-    """
-    for domain in sorted(domains, key=lambda x: x[0]):
-        print(domain[0], domain[1])
-
-
 def count_downloadable_images(posts):
     """
     Count images from post that have direct download link available.
