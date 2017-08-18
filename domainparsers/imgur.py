76f6c618de873aeb7d628f0e84351523b2616438
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-18 14:38:59 +0200

    Add imgur module

---
+++ domainparsers/imgur.py
@@ -0,0 +1,289 @@
+#!/usr/bin/python3
+
+
+"""
+This module contains classes for parsing the imgur.com site. It consists of three
+classes:
+
+~ ImgurException
+~ ImgurFileFormats
+~ Imgur
+
+Imgur is the main class and it obtains list of direct image urls that could be
+used to download images. Example usage:
+
+```python3
+imgur = Imgur('http://imgur.com/gallery/vTTHZ')
+imgur.prepare_images()
+images = imgur.images
+```
+
+imgur.images is a deque of two keyed dictionaries. Example usage:
+
+```python3
+for image in images:
+    print(image['url'], image['filename'])
+```
+
+If images need to be downloaded in order they appear in an album, their filenames
+have to be numerated. Full examples:
+
+```python3
+imgur = Imgur('http://imgur.com/gallery/vTTHZ')
+imgur.prepare_images()
+imgur.images # These are not guaranteed to appear in order when downloaded
+
+imgur.numerate_images()
+images = imgur.images
+```
+"""
+
+
+import re
+from collections import deque
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+
+__version__ = 'v0.2'
+__status__ = 'Development'
+
+
+class ImgurException(Exception):
+    """
+    This exception is raised if supplied link is invalid.
+    """
+    pass
+
+
+class ImgurFileFormats(object):
+    """
+    Contains extensions for file formats that are allowed on imgur. Source:
+
+    https://help.imgur.com/hc/en-us/articles/115000083326-What-files-can-I-upload-
+
+    Archived:
+    http://archive.is/89Uky
+    https://web.archive.org/web/20170222111303/https://help.imgur.com/hc/en-us/articles/115000083326-What-files-can-I-upload-
+    """
+    JPG = '.jpg'
+    JPEG = '.jpeg'
+    PNG = '.png'
+    GIF = '.gif'
+    APNG = '.apng'
+    TIFF = '.tiff'
+    PDF = '.pdf'
+    XCF = '.xcf'
+    #WEBM = '.webm'
+    #MP4 = '.mp4'
+
+    @classmethod
+    def formats(cls):
+        """
+        Return a set consisting of all class attributes. Class attributes must
+        not be callable.
+        """
+        formats = set()
+        for attribute in ImgurFileFormats.__dict__.keys():
+            if attribute[:2] != '__':
+                value = getattr(ImgurFileFormats, attribute)
+                if not callable(value):
+                    formats.add(value)
+        return formats
+
+
+class Imgur(object):
+    """
+    Imgur contains all necessary methods to extract image or album images from
+    imgur link.
+    """
+
+    def __init__(self, url):
+        """
+        Initiate Imgur object.
+        """
+        self.url = self.sanitize(url)
+        self.images = deque()
+
+    def sanitize(self, url):
+        """
+        Check if the supplied link is valid. If not, raise ImgurException. This
+        method checks only if the domain is valid.
+        """
+        if re.match('https?\:\/\/(i\.)?imgur\.com\/', url):
+            if self.is_it_gifv(url):
+                return self.sanitize_gifv(url)
+            return url
+        raise ImgurException('Invalid link.')
+
+    def sanitize_gifv(self, url):
+        """
+        Remove 'v' from .gifv
+        """
+        pattern = 'https?\:\/\/i\.imgur\.com\/[a-zA-Z0-9]+\.gif'
+        return re.match(pattern, url).group(0)
+
+    def is_it_gifv(self, url):
+        """
+        Check if the supplied link points to .gifv page.
+        """
+        if '.gifv' in url:
+            return True
+        return False
+
+    def is_it_image(self):
+        """
+        Check if the url points to image. Examples:
+
+        http(s)://i.imgur.com/[image_hash].[extension]
+        http(s)://i.imgur.com/[image_hash]
+        http(s)://imgur.com/[image_hash]
+        """
+        # https*\:\/\/(i\.)?imgur\.com\/[a-zA-Z0-9]*(\.[a-zA-Z]{1,4})?
+        return not self.is_it_album()
+
+    def is_it_album(self):
+        """
+        Check if the url points to an album. Examples:
+
+        http(s)://imgur.com/a/[album_hash]
+        http(s)://imgur.com/gallery/[album_hash]
+        """
+        return ('/a/' in self.url) or ('/gallery/' in self.url)
+
+    def is_it_grid(self):
+        """
+        Check if the url points to a grid view. Example:
+
+        http(s)://imgur.com/a/[album_hash]?grid
+        """
+        return self.url.endswith('?grid')
+
+    def change_gallery(self):
+        """
+        Change /gallery/ to /a/ in url.
+        """
+        return self.url.replace('/gallery/', '/a/')
+
+    def turn_into_grid(self):
+        """
+        Append ?grid to url.
+        """
+        if self.is_it_album():
+            if not self.is_it_grid():
+                return ''.join([self.change_gallery(), '?grid'])
+            else:
+                return self.url
+        raise ImgurException('Cannot convert single image into album grid.')
+
+    def prepare_images(self):
+        """
+        Parses HTML from the provided url to obtain link(s) to image(s). Raises
+        exception if the link already ends with an extension.
+        """
+        if self.is_it_image():
+            if self.contains_extension(self.url):
+                self.images.append(
+                        self.pack_image(self.url, self.get_image_filename(self.url))
+                    )
+                return
+            else:
+                self.parse_and_prepare_images(self.url)
+                return
+        grid = self.turn_into_grid()
+        self.parse_and_prepare_images(grid)
+        return
+
+    def parse_and_prepare_images(self, url):
+        """
+        Obtain and parse html, and append image dictionaries to image deque.
+        """
+        pattern = '\{"hash":"([a-zA-Z0-9]+)".*?"ext":"([\.a-zA-Z0-9]+)".*?\}'
+        try:
+            html = urlopen(url).read().decode('utf-8')
+            filenames_with_duplicates = re.findall(pattern, html)
+            filenames_clean = self.remove_duplicates(filenames_with_duplicates)
+            urls = self.build_image_url_list(filenames_clean)
+            for url in urls:
+                self.images.append(
+                    self.pack_image(url, self.get_image_filename(url))
+                )
+        except HTTPError as e:
+            print(e.status)
+        except URLError as e:
+            print(e.reason)
+
+    def build_image_url_list(self, filenames):
+        """
+        Build list of direct links to images. Input filenames list is a list of
+        tuples e.g. [('jedEzFL', '.jpg'), ('lciC5G8', '.jpg')]. The output looks
+        like:
+        ['https://i.imgur.com/jedEzFL.jpg', 'https://i.imgur.com/lciC5G8.jpg']
+        """
+        urls = []
+        for filename, extension in filenames:
+            urls.append(''.join(['https://i.imgur.com/', filename, extension]))
+        return urls
+
+    def remove_duplicates(self, filenames):
+        """
+        Remove duplicates from a list of tuples containing filenames with
+        extensions.
+        """
+        clean = []
+        for filename in filenames:
+            if filename not in clean:
+                clean.append(filename)
+        return clean
+
+    def contains_extension(self, url):
+        """
+        Check if the image url contains extension. If there is an extension it
+        is returned. Otherwise, None is returned.
+        """
+        for extension in ImgurFileFormats.formats():
+            if extension in url:
+                return extension
+        return None
+
+    def get_image_filename(self, url):
+        """
+        Get image file name from its url. Examples:
+
+        https://i.imgur.com/jedEzFL.jpg     ->  jedEzFL.jpg
+        https://i.imgur.com/jedEzFL.jpg?1   ->  jedEzFL.jpg
+        """
+        candidate = url.split('/')[-1]
+        extension = self.contains_extension(url)
+        pattern = ''.join(['.+\\', extension])
+        return re.match(pattern, candidate).group(0)
+
+    def pack_image(self, url, filename):
+        """
+        Returns a dictionary with image url and corresponding filename.
+        """
+        return {'url' : url, 'filename' : filename}
+
+    def number_of_images(self):
+        """
+        Get the number of images from the images attribute.
+        """
+        return len(self.images)
+
+    def numerate_images(self):
+        """
+        Append ordinal number to image filename.
+        """
+        total = self.digits_in_a_number(len(self.images))
+        ordinal = '{0:0%dd}' % total
+        for index, image in enumerate(self.images, start=1):
+            image['filename'] = ''.join([
+                    ordinal.format(index), '-', image['filename']
+                ])
+
+    def digits_in_a_number(self, number):
+        """
+        Return how many digits are there in a number.
+        """
+        return len(str(number))
---
+++ tests/test_imgur.py
@@ -0,0 +1,268 @@
+#!/usr/bin/python3
+
+
+import pytest
+from collections import deque
+from domainparsers.imgur import Imgur
+from domainparsers.imgur import ImgurException
+
+
+@pytest.fixture
+def single_image_i():
+    return Imgur('http://i.imgur.com/jedEzFL.jpg')
+
+@pytest.fixture
+def single_image_without_extension_i():
+    return Imgur('http://i.imgur.com/jedEzFL')
+
+@pytest.fixture
+def single_image_without_extension():
+    return Imgur('http://imgur.com/jedEzFL')
+
+@pytest.fixture
+def album_a():
+    return Imgur('http://imgur.com/a/vTTHZ')
+
+@pytest.fixture
+def album_grid():
+    return Imgur('http://imgur.com/a/vTTHZ?grid')
+
+@pytest.fixture
+def album_gallery():
+    return Imgur('http://imgur.com/gallery/vTTHZ')
+
+def test_invalid_link():
+    """
+    Test if the class raises exception for invalid link.
+    """
+    with pytest.raises(ImgurException):
+        Imgur('https://www.reddit.com')
+
+def test_invalid_link_with_variation():
+    """
+    Test if the class raises exception for invalid link.
+    """
+    with pytest.raises(ImgurException):
+        Imgur('https://www.reddit.com/imgur.com')
+
+def test_invalid_link_almost_correct():
+    """
+    Test if the class raises exception for invalid link.
+    """
+    with pytest.raises(ImgurException):
+        Imgur('http://.iimgur.com/jedEzFL')
+
+def test_invalid_link_with_multiple_s():
+    """
+    Test if the class raises exception for invalid link.
+    """
+    with pytest.raises(ImgurException):
+        Imgur('httpssssss://i.imgur.com/jedEzFL')
+
+def test_is_it_image_with_extension(single_image_i):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert single_image_i.is_it_image() == True
+
+def test_is_it_image_without_extension_i(single_image_without_extension_i):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert single_image_without_extension_i.is_it_image() == True
+
+def test_is_it_image_without_extension(single_image_without_extension):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert single_image_without_extension.is_it_image() == True
+
+def test_is_it_album_image_with_extension(single_image_i):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert single_image_i.is_it_album() == False
+
+def test_is_it_album_image_without_extension_i(single_image_without_extension_i):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert single_image_without_extension_i.is_it_album() == False
+
+def test_is_it_album_image_without_extension(single_image_without_extension):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert single_image_without_extension.is_it_album() == False
+
+def test_is_it_image_album_a(album_a):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert album_a.is_it_image() == False
+
+def test_is_it_image_album_gallery(album_gallery):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert album_gallery.is_it_image() == False
+
+def test_is_it_album_a(album_a):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert album_a.is_it_album() == True
+
+def test_is_it_album_gallery(album_gallery):
+    """
+    Test if the provided URL leads to an image or album.
+    """
+    assert album_gallery.is_it_album() == True
+
+def test_if_album_ends_with_grid_1(album_a):
+    """
+    Test if the provided link ends with '?grid'
+    """
+    assert album_a.is_it_grid() == False
+
+def test_if_album_ends_with_grid_2(album_grid):
+    """
+    Test if the provided link ends with '?grid'
+    """
+    assert album_grid.is_it_grid() == True
+
+def test_gallery_to_a(album_gallery, album_a):
+    """
+    Change /gallery/ to /a/
+    """
+    assert album_gallery.change_gallery() == album_a.url
+
+def test_turn_album_into_grid(album_gallery, album_a, album_grid):
+    """
+    Turn album into grid.
+    """
+    assert album_gallery.turn_into_grid() == album_grid.url
+    assert album_a.turn_into_grid() == album_grid.url
+    assert album_grid.turn_into_grid() == album_grid.url
+
+def test_raise_exception_turn_into_grid(single_image_i):
+    """
+    Raise exception if the provided link cannot be turned into grid.
+    """
+    with pytest.raises(ImgurException):
+        single_image_i.turn_into_grid()
+
+@pytest.mark.skip(reason='no way of currently testing this')
+def test_get_image(single_image_without_extension):
+    """
+    Get a direct link to a single image.
+    """
+    result = deque({
+        'url' : 'http://i.imgur.com/jedEzFL.jpg',
+        'filename' : 'jedEzFL.jpg',
+    })
+    single_image_without_extension.prepare_images()
+    assert single_image_without_extension.images == result
+
+@pytest.mark.skip(reason='no way of currently testing this')
+def test_get_images(album_gallery):
+    """
+    Get images from an album.
+    """
+    result = deque({
+        'url' : 'http://i.imgur.com/lciC5G8.jpg',
+        'filename' : 'lciC5G8.jpg',
+    }, {
+        'url' : 'http://i.imgur.com/6O1hqeY.jpg',
+        'filename' : '6O1hqeY.jpg',
+    })
+    album_gallery.prepare_images()
+    assert album_gallery.images == result
+
+def test_contains_extension(single_image_i, single_image_without_extension):
+    """
+    Check if the extension is contained in image url.
+    """
+    assert single_image_i.contains_extension(single_image_i.url) == '.jpg'
+    assert single_image_without_extension.contains_extension(
+            single_image_without_extension.url
+        ) == None
+
+def test_get_image_filename(single_image_i):
+    """
+    Check if the method returns filename obtained from the direct image url.
+    """
+    assert single_image_i.get_image_filename(single_image_i.url) == 'jedEzFL.jpg'
+
+def test_duplicates(single_image_without_extension, album_gallery):
+    """
+    Remove duplicates from matches returned from regex.
+    """
+    candidate_1 = [('jedEzFL', '.jpg'), ('jedEzFL', '.jpg')]
+    candidate_2 = [('jedEzFL', '.jpg'), ('lciC5G8', '.jpg'),
+                   ('jedEzFL', '.jpg'), ('lciC5G8', '.jpg')]
+    assert single_image_without_extension.remove_duplicates(candidate_1) == [('jedEzFL', '.jpg')]
+    assert album_gallery.remove_duplicates(candidate_2) == [('jedEzFL', '.jpg'), ('lciC5G8', '.jpg')]
+
+def test_build_image_links(album_gallery):
+    """
+    Test if the input list of filename tuples returns direct links to images.
+    """
+    candidate = [('jedEzFL', '.jpg'), ('lciC5G8', '.jpg')]
+    result = ['https://i.imgur.com/jedEzFL.jpg', 'https://i.imgur.com/lciC5G8.jpg']
+    assert album_gallery.build_image_url_list(candidate) == result
+
+def test_number_of_images(album_gallery):
+    """
+    Test the number of images contained in 'images' attribute.
+    """
+    assert album_gallery.number_of_images() == 0
+
+def test_check_ordering(album_gallery):
+    """
+    Check if the image filenames have appended ordinal numbers.
+    """
+    candidates = deque([
+        {'url' : 'https://i.imgur.com/jedEzFL.jpg', 'filename' : 'jedEzFL.jpg',},
+        {'url' : 'https://i.imgur.com/lciC5G8.jpg', 'filename' : 'lciC5G8.jpg',},
+    ])
+    result = deque([
+        {'url' : 'https://i.imgur.com/jedEzFL.jpg', 'filename' : '1-jedEzFL.jpg',},
+        {'url' : 'https://i.imgur.com/lciC5G8.jpg', 'filename' : '2-lciC5G8.jpg',},
+    ])
+
+    album_gallery.images = candidates
+    album_gallery.numerate_images()
+    assert album_gallery.images == result
+
+def test_digits_in_a_number(album_gallery):
+    """
+    Test to see how many digits are there in an album.
+    """
+    assert album_gallery.digits_in_a_number(7) == 1
+    assert album_gallery.digits_in_a_number(12) == 2
+    assert album_gallery.digits_in_a_number(1454) == 4
+
+def test_if_link_contains_gifv(single_image_i):
+    """
+    Test if the provided link contains .gifv extension.
+    """
+    url_1 = 'http://i.imgur.com/jedEzFL.gif'
+    url_2 = 'http://i.imgur.com/jedEzFL.giv'
+    url_3 = 'http://i.imgur.com/jedEzFL.gifv'
+    url_4 = 'http://i.imgur.com/jedEzFL.gifv?1'
+    assert single_image_i.is_it_gifv(single_image_i.url) == False
+    assert single_image_i.is_it_gifv(url_1) == False
+    assert single_image_i.is_it_gifv(url_2) == False
+    assert single_image_i.is_it_gifv(url_3) == True
+    assert single_image_i.is_it_gifv(url_4) == True
+
+def test_sanitize_gifv(single_image_i):
+    """
+    Test if sanitize_gifv method returns a proper link to a gif.
+    """
+    url_1 = 'http://i.imgur.com/jedEzFL.gifv'
+    url_2 = 'http://i.imgur.com/jedEzFL.gifv?1'
+    result = 'http://i.imgur.com/jedEzFL.gif'
+    assert single_image_i.sanitize_gifv(url_1) == result
+    assert single_image_i.sanitize_gifv(url_2) == result
